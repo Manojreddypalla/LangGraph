80d21d148c104ec9a0339994c25bda809e0659c4
Manoj Reddy <manojreddypalla@gmail.com>
2026-02-07 17:35:11 +0530

    tb

--- chatbot.py
+++ chatbot.py
@@ -3,6 +3,13 @@ from typing import Annotated, List
 from langgraph.graph.message import add_messages
 from langgraph.graph import StateGraph, START,END
 from langchain_ollama import ChatOllama
+from openai import OpenAI
+from dotenv import load_dotenv
+from typing import Optional, Literal
+import os 
+
+load_dotenv()
+GEMINI_API_KEY=os.getenv("GEMINI_API_KEY")
 
 llm = ChatOllama(
     model="llama3",
@@ -10,6 +17,10 @@ llm = ChatOllama(
     # other params...
 )
 
+client = OpenAI(
+    api_key=GEMINI_API_KEY,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+)
 
 
 class State(TypedDict):
@@ -21,6 +32,34 @@ def chatbot(state:State):
     return{"messages":[responce]}
 
 
+def gemini(state: State):
+
+    print("chatbot_gemini Node", state)
+    response = client.chat.completions.create(
+        model="gemini-2.5-flash",
+        messages=[
+        
+        {
+            "role": "user",
+            "content": "Explain to me how AI works"
+        }
+    ]
+  )
+    state["llm_output"] = response.choices[0].message.content
+    return state
+
+
+def evalaute_response(state: State) -> Literal["gemini", "endnode"]:
+    print("evalaute_response Node", state)
+    if False:
+        return "endnode"
+    
+    return "gemini"
+
+def endnode(state: State):
+    print("endnode Node", state)
+    return state
+
 def sample(state:State):
     print("inside sample node for nowww")
     return {"message":["sample appended "]}
@@ -30,9 +69,16 @@ graph_builder = StateGraph(State)
 
 graph_builder.add_node("chatbot",chatbot)
 graph_builder.add_node("sample",sample)
+graph_builder.add_node("gemini",gemini)
+graph_builder.add_node("endnode", endnode)
+
+
 
 graph_builder.add_edge(START,"chatbot")
-graph_builder.add_edge("chatbot","sample")
+
+# graph_builder.add_edge("chatbot","sample")
+graph_builder.add_conditional_edges("chatbot",evalaute_response)
+graph_builder.add_edge("gemini","endnode")
 
 
 graph = graph_builder.compile()
